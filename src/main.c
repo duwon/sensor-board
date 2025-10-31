6adb12b834e9520df7caed0dcdbb111124e19968
duwon <duwon@duwon.net>
2025-10-31 18:03:51 +0900

    확장 광고 테스트 완료
    - 버튼 짧게 누르면 확장 광고
    - 길게 누르면 스캔 응답

--- src/main.c
+++ src/main.c
@@ -124,10 +124,13 @@ static void loop_fn(struct k_work *w)
     if (btn == BTN_EVT_LONG)
     {
         LOG_INF("Long button press detected");
+        Init_Ble(BLE_SCAN_RESPONSE); /* 스캔 응답 설정 */
+        return;
     }
     else if (btn == BTN_EVT_SHORT)
     {
         LOG_INF("Short button press detected");
+        Init_Ble(BTN_ADV);          /* 확장 광고 설정 */
     }
     else if (btn == BTN_EVT_NONE)
     {
@@ -192,8 +195,8 @@ int main(void)
 
     /* BLE 초기화 */
     Init_Ble(BLE_INIT);
-    Init_Ble(BLE_SCAN_RESPONSE); /* 스캔 응답 설정 */
-    // Init_Ble(BTN_ADV);          /* 확장 광고 설정 */
+    // Init_Ble(BLE_SCAN_RESPONSE); /* 스캔 응답 설정 */
+    Init_Ble(BTN_ADV);          /* 확장 광고 설정 */
 
     /* 디버깅 코드 실행 */
     debug_run_startup();
@@ -203,8 +206,8 @@ int main(void)
     k_work_schedule(&led_work, K_MSEC(200));
 
     /* 메인 루프 작업 초기화 및 1초 후 시작 */
-    // k_work_init_delayable(&loop_work, loop_fn);
-    // k_work_schedule(&loop_work, K_SECONDS(1));
+    k_work_init_delayable(&loop_work, loop_fn);
+    k_work_schedule(&loop_work, K_SECONDS(1));
 
     return 0;
 }
